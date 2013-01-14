e730a8a3c17a4113844bedd1feab880d38ffc648
Anthony C <agcarat@gmail.com>
2013-01-14 01:55:00 -0500

    removed some ruff draft implementations

--- include/zeromqb/zeromqb.hpp
+++ include/zeromqb/zeromqb.hpp
@@ -10,66 +10,6 @@ typedef unsigned int SubscriberMask_t;
 
 namespace ZeroMQb {
 
-class MessageQueueInteraface {
-	public:
-		virtual ~MessageQueueInteraface() { }
-		virtual void writeMessage(const std::vector<char> & message) = 0;
-		virtual bool readMessage(std::vector<char> & message) = 0;
-		virtual bool isFull() = 0;
-		virtual bool isEmpty() = 0;
-		virtual size_t count() = 0;
-
-	private:
-};
-
-class MessageQueue :MessageQueueInteraface{
-	public:
-		MessageQueue(MessageQueueInteraface &mq) : mq_(mq) {}
-		virtual ~MessageQueue() { }
-		void writeMessage(const std::vector<char> & message)  {
-			mq_.writeMessage(message);
-		}
-
-		bool readMessage(std::vector<char> & message)  {
-			mq_.readMessage(message);
-			return true;	
-		}
-		bool isFull() {return mq_.isFull();}
-		bool isEmpty() {return mq_.isEmpty();}
-		size_t count () {
-			return mq_.count();
-		}
-	private:
-		MessageQueueInteraface &mq_; 
-};
-
-//You should only use this with one thread reading and another writting because  isFull and isEmpty is not in a transational lock with read and write  message
-//
-class MessageQueue_threadsafe: MessageQueueInteraface {
-	public:
-		MessageQueue_threadsafe(MessageQueueInteraface &mq_internal) : mq_(mq_internal),mutex_(){
-		}
-		virtual ~MessageQueue_threadsafe() { }
-		
-		void writeMessage(std::vector<char> & message)  {
-			boost::mutex::scoped_lock lock(mutex_);
-			mq_.writeMessage(message);
-		}
-
-		bool readMessage(std::vector<char> & message)  {
-			mq_.readMessage(message);
-			return true;
-		}
-		bool isFull() {return mq_.isFull();}
-		bool isEmpty() {return mq_.isEmpty();}
-		size_t count() {
-			return mq_.count();
-		}
-	private:
-		MessageQueue mq_;
-		boost::mutex mutex_;
-};
-
 template <class T> 
 class GlobalSubscriptionManager {
 	public:
